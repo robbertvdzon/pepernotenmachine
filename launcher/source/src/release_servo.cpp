91e62b5720ece91af0fc27861a9ec1e75ecf0364
Ferry Spiering <ferry.spiering@gmail.com>
2026-09-16 11:50:01 +0200

    Add state led to the rellease servo

--- launcher/source/src/release_servo.cpp
+++ launcher/source/src/release_servo.cpp
@@ -1,7 +1,10 @@
 #include "../include/release_servo.h"
 #include "../include/config.h"
+#include "../include/led.h"
 #include <Arduino.h>
 #include <ESP32Servo.h>
+
+#define STATE_LED LED_3
 
 static Servo servo;
 static uint8_t currentAngle = 0;
@@ -9,6 +12,7 @@ static uint8_t currentAngle = 0;
 void release_servo_init() {
     servo.attach(RELEASE_SERVO_PIN);
     servo.write(currentAngle);
+    led_set_state(STATE_LED, LED_OFF);
 }
 
 void release_servo_set_angle(uint8_t angle) {
@@ -17,6 +21,8 @@ void release_servo_set_angle(uint8_t angle) {
     }
     currentAngle = angle;
     servo.write(angle);
+    led_set_state(STATE_LED, LED_ON);
+    led_set_state(STATE_LED, LED_RAMP_DOWN);
     Serial.print("Servo angle set to ");
     Serial.print(angle);
     Serial.println(" degrees");
